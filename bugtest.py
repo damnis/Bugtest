d1b278d8267697e5fd481d7ce825cfeb2fae81fa
damnis <36800988+damnis@users.noreply.github.com>
2025-06-28 01:15:40 +0200

    Update bugtest.py

--- bugtest.py
+++ bugtest.py
@@ -1,5 +1,8 @@
 import yfinance as yf
+import streamlit as st
 
+st.title("Test Streamlit App")
+st.write("Hallo wereld!")
 # Haal recente data op
 df = yf.download("AAPL", period="1mo", interval="1d")
 
@@ -8,3 +11,4 @@ df["Close_shift1"] = df["Close"].shift(1)
 df["Close_shift2"] = df["Close"].shift(2)
 
 print(df[["Close", "Close_shift1", "Close_shift2"]].tail(10))
+print(df[["Close", "Close_shift1", "Close_shift1"]].tail(10))
